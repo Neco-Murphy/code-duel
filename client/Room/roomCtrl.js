61d9e00da3c553aefd229e1c50c6c8320ef74796
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-10-30 16:33:46 -0700

    corrected beginning countdown time

--- client/Room/roomCtrl.js
+++ client/Room/roomCtrl.js
@@ -1,7 +1,7 @@
 angular.module('app')
   .controller('roomCtrl', function($scope, $log, $timeout, socket) {
     $scope.sent = false;
-    var clock = $('.clock').FlipClock(10, {
+    var clock = $('.clock').FlipClock(600, {
       clockFace: 'MinuteCounter',
       autoStart: false,
       countdown: true,
@@ -13,7 +13,7 @@ angular.module('app')
             //disable submit and reset button
             $('.submitButton').prop('disabled', true);
             $('.resetButton').prop('disabled', true);
-          } 
+          }
         }
       }
     });
